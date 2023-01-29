94bbac48ae567f46cc4e81f954ebccfeb7a6c8de
josh! <github@joshhighet.com>
2023-01-29 18:16:14 +1300

    use sed, no bash stream parsing on ash

--- tor/tor-hushcontrolport.sh
+++ tor/tor-hushcontrolport.sh
@@ -1,7 +1,8 @@
 #!/bin/ash
-tor -f /etc/tor/torrc \
-| grep -v "New control connection opened from" \
-| grep -v "Tor can't help you if you use it wrong" \
-| grep -v "Read configuration file" \
-| grep -v "You have a ControlPort set to accept connections from a non-local address." \
-| grep -v "You specified a public address '0.0.0.0:9050' for SocksPort."
+tor -f /etc/tor/torrc
+#| sed \
+#-e "/New control connection opened from/d" \
+#-e "/You have a ControlPort set to accept connections from a non-local address/d" \
+#-e "/Tor can't help you if you use it wrong/d" \
+#-e "/Read configuration file/d" \
+#-e "/You specified a public address '0.0.0.0:9050' for SocksPort./d"
