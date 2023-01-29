494c0a4fd8b04e4c389e14ac702e4ba910927543
josh! <github@joshhighet.com>
2023-01-29 13:20:18 +1300

    allow local testing on macos

--- haconfig/gen_conf.py
+++ haconfig/gen_conf.py
@@ -1,10 +1,13 @@
 import os
 import docker
+import platform
 from jinja2 import Template
 
 def get_tor_ips():
-    client = docker.DockerClient(base_url='unix://tmp/docker.sock')
-    # client = docker.DockerClient(base_url='unix://var/run/docker.sock')
+    if platform.system() == "Darwin":
+        client = docker.DockerClient(base_url='unix://var/run/docker.sock')
+    else:
+        client = docker.DockerClient(base_url='unix://tmp/docker.sock')
     network = client.networks.get("net_tor")
     net_tor_id = network.attrs["Id"]
     containers = client.containers.list()
@@ -22,6 +25,10 @@ if __name__ == "__main__":
     tor_ips = get_tor_ips()
     with open("haproxy.j2", "r") as file:
         conf = Template(file.read()).render(tor_hosts=tor_ips)
-    with open("/usr/local/etc/haproxy/haproxy.cfg", "w") as file:
+    if platform.system() == "Darwin":
+        outfile = "haproxy-valid.cfg"
+    else:
+        outfile = '/usr/local/etc/haproxy/haproxy.cfg'
+    with open(outfile, "w") as file:
         file.write(conf)
 
