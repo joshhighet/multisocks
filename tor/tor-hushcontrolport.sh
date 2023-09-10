9c31c6a7042f9f8a5f103162abd9b35c9db32245
josh! <17993143+joshhighet@users.noreply.github.com>
2023-09-10 21:24:19 +1200

    Delete tor-hushcontrolport.sh

--- tor/tor-hushcontrolport.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/ash
-tor -f /etc/tor/torrc
-#| sed \
-#-e "/New control connection opened from/d" \
-#-e "/You have a ControlPort set to accept connections from a non-local address/d" \
-#-e "/Tor can't help you if you use it wrong/d" \
-#-e "/Read configuration file/d" \
-#-e "/You specified a public address '0.0.0.0:9050' for SocksPort./d"
