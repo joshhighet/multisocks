17c7a71ed81e3de66ccb9120efebe7619e3936ab
josh! <github@joshhighet.com>
2025-07-11 13:05:34 +1200

    Update route.ts

--- dashboard/app/api/stats/route.ts
+++ dashboard/app/api/stats/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  const url = 'http://multisocks.dark:1337/;csv';
+  const url = 'http://localhost:1337/;csv';
   const res = await fetch(url);
   const text = await res.text();
   return NextResponse.json({ csv: text });
